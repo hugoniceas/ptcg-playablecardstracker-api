b0475429cbf55e3458b5a6fa52e17c614cd0b64b
hugoniceas <niceasoriginal@gmail.com>
2026-03-05 20:17:43 -0300

    created getLimitlessTournamentPlayers function

--- src/services/limitlessScraperService.ts
+++ src/services/limitlessScraperService.ts
@@ -11,6 +11,12 @@ interface Tournament {
     limitlessLink: string;
 }
 
+interface TournamentPlayer {
+    name: string;
+    place: number;
+    limitlessLink: string;
+}
+
 async function getLimitlessTournaments(): Promise<Tournament[]> {
     try {
         const response = await axios.get(limitless_url + '/tournaments');
@@ -43,4 +49,35 @@ async function getLimitlessTournaments(): Promise<Tournament[]> {
     }
 }
 
-export { getLimitlessTournaments };
+// Testar oque acontece quando n_players é maior que o número de jogadores do torneio
+async function getLimitlessTournamentPlayers(tournamentLink: string, n_players: number): Promise<TournamentPlayer[]> {
+    try {
+        const response = await axios.get(limitless_url + tournamentLink);
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const players: TournamentPlayer[] = [];
+
+        $('table tbody tr:not(:first-child)').each((i, element) => {
+            if (i < n_players) {
+                const player_element = $(element);
+
+                const player = {
+                    name: player_element.attr('data-name') || '',
+                    place: parseInt(player_element.attr('data-rank') || '0', 10),
+                    limitlessLink: player_element.find('td:nth-child(5) a').attr('href') || ''
+                }
+                players.push(player);
+            }
+        })
+
+        console.log(players);
+        return players;
+    } catch (error) {
+        console.error('Error fetching Limitless tournament players:', error);
+        return [];
+    }
+}
+
+getLimitlessTournamentPlayers('/tournaments/542', 16);
+
+export { getLimitlessTournaments, getLimitlessTournamentPlayers };
